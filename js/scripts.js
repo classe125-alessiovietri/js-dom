3790ff8ce46903c204f40a53d870f21e7a3e5283
Alessio Vietri <info@alessiovietri.it>
2024-04-18 11:57:52 +0200

    Completata parte mancante lezione precedente

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+const pippo = new Date();
+
+console.log(pippo);
+console.log(pippo.getHours());
+console.log(pippo.getMinutes());
+console.log(pippo.getSeconds());
+console.log(pippo.getFullYear());
+
+
+/* 
+--------------------------------
+*/
+// Genero un numero random compreso tra 0 e 1 (1 escluso)
+const randomNumber = Math.random();
+console.log(randomNumber);
+
+	
+// Restituisce un numero tra 1 e 100 
+console.log(Math.floor(Math.random() * 100) + 1);
